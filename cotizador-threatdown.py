8091c32949357b6165ac9c002dbcd67272a8f853
B10sp4rt4n <salvador_ruiz74@hotmail.com>
2025-02-10 11:37:15 -0600

    Update cotizador-threatdown.py
    Escoge entre productos solamente ThD

--- cotizador-threatdown.py
+++ cotizador-threatdown.py
@@ -17,25 +17,31 @@ def main():
     df_productos = cargar_datos()
 
     if df_productos is not None:
-        # Verificar si la columna 'Term (Month)' existe
-        if 'Term (Month)' in df_productos.columns:
+        # Verificar si las columnas necesarias existen
+        if 'Term (Month)' in df_productos.columns and 'Product Title' in df_productos.columns:
             # Crear un selectbox para el período de duración
             periodo = st.selectbox(
                 'Selecciona el período de duración (meses):',
                 options=[12, 24, 26]
             )
 
-            # Filtrar el DataFrame según el período seleccionado
-            df_filtrado = df_productos[df_productos['Term (Month)'] == periodo]
+            # Definir las palabras clave para filtrar
+            palabras_clave = ['CORE', 'CORE SERVER', 'ADVANCED', 'ADVANCED SERVER', 'ELITE', 'ELITE SERVER', 'ULTIMATE', 'ULTIMATE SERVER', 'MOBILE']
+            patron_busqueda = '|'.join(palabras_clave)
+
+            # Filtrar el DataFrame según el período seleccionado y las palabras clave en 'Product Title'
+            df_filtrado = df_productos[
+                (df_productos['Term (Month)'] == periodo) &
+                (df_productos['Product Title'].str.contains(patron_busqueda, case=False, na=False))
+            ]
 
             # Mostrar los datos filtrados
-            st.write(f"Productos con un período de {periodo} meses:")
+            st.write(f"Productos con un período de {periodo} meses y títulos que contienen las palabras clave:")
             st.dataframe(df_filtrado)
         else:
-            st.error("La columna 'Term (Month)' no se encuentra en el archivo.")
+            st.error("Las columnas 'Term (Month)' y/o 'Product Title' no se encuentran en el archivo.")
     else:
         st.write("No se pudieron cargar los datos.")
 
 if __name__ == "__main__":
     main()
-
