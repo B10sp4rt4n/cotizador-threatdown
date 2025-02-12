26ee2c6480152af2d874909603447c44acd1d9b4
B10sp4rt4n <salvador_ruiz74@hotmail.com>
2025-02-11 18:33:48 -0600

    Update cotizador-threatdown.py

--- cotizador-threatdown.py
+++ cotizador-threatdown.py
@@ -25,32 +25,32 @@ def main():
                 options=[12, 24, 26]
             )
 
-            # Definir las palabras clave disponibles para filtrar
-            palabras_clave_disponibles = ['CORE', 'CORE SERVER', 'ADVANCED', 'ADVANCED SERVER', 'ELITE', 'ELITE SERVER', 'ULTIMATE', 'ULTIMATE SERVER', 'MOBILE']
+            # Definir las opciones de licencias disponibles para filtrar
+            opciones_licencias = ['CORE', 'CORE SERVER', 'ADVANCED', 'ADVANCED SERVER', 'ELITE', 'ELITE SERVER', 'MOBILE']
 
-            # Crear un multiselect para que el usuario seleccione las palabras clave deseadas
-            palabras_clave_seleccionadas = st.multiselect(
-                'Selecciona las palabras clave para filtrar los productos:',
-                options=palabras_clave_disponibles,
-                default=palabras_clave_disponibles  # Puedes cambiar esto según tus necesidades
+            # Crear un multiselect para que el usuario seleccione las licencias deseadas
+            licencias_seleccionadas = st.multiselect(
+                'Selecciona las opciones de licencia para filtrar los productos:',
+                options=opciones_licencias,
+                default=opciones_licencias  # Puedes cambiar esto según tus necesidades
             )
 
-            # Verificar que se haya seleccionado al menos una palabra clave
-            if palabras_clave_seleccionadas:
-                # Crear un patrón de búsqueda uniendo las palabras clave seleccionadas con el operador OR '|'
-                patron_busqueda = '|'.join(palabras_clave_seleccionadas)
+            # Verificar que se haya seleccionado al menos una opción de licencia
+            if licencias_seleccionadas:
+                # Crear un patrón de búsqueda uniendo las licencias seleccionadas con el operador OR '|'
+                patron_busqueda = '|'.join(licencias_seleccionadas)
 
-                # Filtrar el DataFrame según el período seleccionado y las palabras clave en 'Product Title'
+                # Filtrar el DataFrame según el período seleccionado y las licencias en 'Product Title'
                 df_filtrado = df_productos[
                     (df_productos['Term (Month)'] == periodo) &
                     (df_productos['Product Title'].str.contains(patron_busqueda, case=False, na=False))
                 ]
 
                 # Mostrar los datos filtrados
-                st.write(f"Productos con un período de {periodo} meses y títulos que contienen las palabras clave seleccionadas:")
+                st.write(f"Productos con un período de {periodo} meses y títulos que contienen las opciones de licencia seleccionadas:")
                 st.dataframe(df_filtrado)
             else:
-                st.warning("Por favor, selecciona al menos una palabra clave para filtrar los productos.")
+                st.warning("Por favor, selecciona al menos una opción de licencia para filtrar los productos.")
         else:
             st.error("Las columnas 'Term (Month)' y/o 'Product Title' no se encuentran en el archivo.")
     else:
