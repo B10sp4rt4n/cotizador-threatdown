9677b49c70177e509ea13ea9c79deba79c55a30e
B10sp4rt4n <salvador_ruiz74@hotmail.com>
2025-02-11 19:11:19 -0600

    Update cotizador-threatdown.py

--- cotizador-threatdown.py
+++ cotizador-threatdown.py
@@ -11,15 +11,28 @@ def cargar_datos():
         return None
 
 def main():
-    st.title("Lista Completa de Productos")
+    st.title("Filtrado de Productos por Título")
 
     # Cargar datos
     df_productos = cargar_datos()
 
     if df_productos is not None:
-        # Mostrar la lista completa de productos
-        st.write("A continuación se muestra la lista completa de productos:")
-        st.dataframe(df_productos)
+        # Definir las palabras clave para el filtrado
+        palabras_clave = ["CORE", "CORE SERVER", "ADVANCED", "ADVANCED SERVER", 
+                          "ELITE", "ELITE SERVER", "ULTIMATE", "ULTIMATE SERVER", "MOBILE"]
+
+        # Crear una expresión regular que combine todas las palabras clave
+        patron = '|'.join(palabras_clave)
+
+        # Filtrar el DataFrame para incluir solo los productos cuyos títulos contienen las palabras clave
+        df_filtrado = df_productos[df_productos['Product Title'].str.contains(patron, case=False, na=False)]
+
+        # Verificar si hay productos después del filtrado
+        if not df_filtrado.empty:
+            st.write("Productos que coinciden con las palabras clave especificadas:")
+            st.dataframe(df_filtrado)
+        else:
+            st.warning("No se encontraron productos que coincidan con las palabras clave especificadas.")
     else:
         st.write("No se pudieron cargar los datos.")
 
