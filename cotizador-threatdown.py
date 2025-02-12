1c45b3366e8553fa53b1318cf8c517c6c902290e
B10sp4rt4n <salvador_ruiz74@hotmail.com>
2025-02-11 18:13:42 -0600

    Update cotizador-threatdown.py

--- cotizador-threatdown.py
+++ cotizador-threatdown.py
@@ -25,19 +25,32 @@ def main():
                 options=[12, 24, 26]
             )
 
-            # Definir las palabras clave para filtrar
-            palabras_clave = ['CORE', 'CORE SERVER', 'ADVANCED', 'ADVANCED SERVER', 'ELITE', 'ELITE SERVER', 'ULTIMATE', 'ULTIMATE SERVER', 'MOBILE']
-            patron_busqueda = '|'.join(palabras_clave)
+            # Definir las palabras clave disponibles para filtrar
+            palabras_clave_disponibles = ['CORE', 'CORE SERVER', 'ADVANCED', 'ADVANCED SERVER', 'ELITE', 'ELITE SERVER', 'ULTIMATE', 'ULTIMATE SERVER', 'MOBILE']
 
-            # Filtrar el DataFrame según el período seleccionado y las palabras clave en 'Product Title'
-            df_filtrado = df_productos[
-                (df_productos['Term (Month)'] == periodo) &
-                (df_productos['Product Title'].str.contains(patron_busqueda, case=False, na=False))
-            ]
+            # Crear un multiselect para que el usuario seleccione las palabras clave deseadas
+            palabras_clave_seleccionadas = st.multiselect(
+                'Selecciona las palabras clave para filtrar los productos:',
+                options=palabras_clave_disponibles,
+                default=palabras_clave_disponibles  # Puedes cambiar esto según tus necesidades
+            )
 
-            # Mostrar los datos filtrados
-            st.write(f"Productos con un período de {periodo} meses y títulos que contienen las palabras clave:")
-            st.dataframe(df_filtrado)
+            # Verificar que se haya seleccionado al menos una palabra clave
+            if palabras_clave_seleccionadas:
+                # Crear un patrón de búsqueda uniendo las palabras clave seleccionadas con el operador OR '|'
+                patron_busqueda = '|'.join(palabras_clave_seleccionadas)
+
+                # Filtrar el DataFrame según el período seleccionado y las palabras clave en 'Product Title'
+                df_filtrado = df_productos[
+                    (df_productos['Term (Month)'] == periodo) &
+                    (df_productos['Product Title'].str.contains(patron_busqueda, case=False, na=False))
+                ]
+
+                # Mostrar los datos filtrados
+                st.write(f"Productos con un período de {periodo} meses y títulos que contienen las palabras clave seleccionadas:")
+                st.dataframe(df_filtrado)
+            else:
+                st.warning("Por favor, selecciona al menos una palabra clave para filtrar los productos.")
         else:
             st.error("Las columnas 'Term (Month)' y/o 'Product Title' no se encuentran en el archivo.")
     else:
